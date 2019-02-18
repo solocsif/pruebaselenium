07364d3bb06fceb6315e0ea57e1d3a9183f7da57
javiercastillo <javiercastillo@PE-HO-ATH-103.PERIFERIAGROUP.MS>
2019-02-18 14:32:54 -0500

    version1

---
+++ src/main/java/com/seldemod/com/seldemod/selenium/App.java
@@ -0,0 +1,13 @@
+package com.seldemod.com.seldemod.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/seldemod/com/seldemod/selenium/seleniumtest.java
@@ -0,0 +1,8 @@
+package com.seldemod.com.seldemod.selenium;
+import org.testng.annotations.Test;
+ class seleniumtest {
+	@Test
+public void printsel() {
+	System.out.println("esta es una prueba de seleniums");
+}
+}
